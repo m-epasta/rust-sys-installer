db80deb787411add63d940a26f5dab3048d89261
m-epasta <mepastaacc@outlook.com>
2025-12-21 19:58:38 +0000

    fixed a missunderstanding about vscode that leaded to an error when installing it

--- src/cli/executor.rs
+++ src/cli/executor.rs
@@ -71,10 +71,7 @@ pub fn install_rust() -> Result<(), ProcessError> {
 
 pub fn install_vscode() -> Result<(), ProcessError> {
     let setup_commands = r#"
-        wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
-        sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
-        echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" | sudo tee /etc/apt/sources.list.d/vscode.list > /dev/null
-        rm packages.microsoft.gpg
+        sudo snap install code --classic
     "#;
 
     CommandBuilder::new("bash")
@@ -82,7 +79,6 @@ pub fn install_vscode() -> Result<(), ProcessError> {
         .arg(setup_commands)
         .execute()?;
 
-    CommandBuilder::apt_install("code").execute()?;
 
     Ok(())
 }
